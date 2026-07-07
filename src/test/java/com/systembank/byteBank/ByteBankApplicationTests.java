b8cdf5e68b59bf1c6f387c2bcdeeba1cd5f939f9
danielpf30 <danielparnaiba1030@gmail.com>
2026-07-06 21:22:57 -0300

    First commit - Configuração inicial e conexão com o Supabase

---
+++ src/test/java/com/systembank/byteBank/ByteBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systembank.byteBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
